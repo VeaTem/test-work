9e244c2244ad0ae600a4bbeb1db748b028f8488b
VeaTem <veatem.web@gmail.com>
2022-04-13 02:29:37 +0300

    desctop done

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,25 @@
-const warning = document.getElementById('warning')
+
+const warning = document.getElementById('warning');
 
 function closeWarning() {
     warning.style.background = "#fff";
     warning.style.left = "-5000px";
-    setTimeout(() =>  warning.style.display = "none", 600);
-   
-
+    setTimeout(() => warning.style.display = "none", 600);
 }
 
+
+
+$(function () {
+    $("#tagCloud a").tagcloud({
+        size: {
+            start: 12,
+            end: 36,
+            unit: "px"
+        },
+        color: {
+            start: '#889099',
+            end: '#000'
+        }
+
+    });
+});
